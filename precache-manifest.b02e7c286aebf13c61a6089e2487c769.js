23b9c0d8f3e18400ccd3970d517099e73ba3b05f
abhijeetsinghtomar <abhijeetsinghemail@gmail.com>
2020-01-12 18:44:23 +0530

    Updates

---
+++ precache-manifest.b02e7c286aebf13c61a6089e2487c769.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b1339136645fde296bf751811b4c61",
+    "url": "/react-cart/index.html"
+  },
+  {
+    "revision": "b21e5936804603b47216",
+    "url": "/react-cart/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "2d4200e036f722db4b8c",
+    "url": "/react-cart/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b21e5936804603b47216",
+    "url": "/react-cart/static/js/2.4cbeeed0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-cart/static/js/2.4cbeeed0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d4200e036f722db4b8c",
+    "url": "/react-cart/static/js/main.eb81ca6e.chunk.js"
+  },
+  {
+    "revision": "8743e4f9d423535bd64b",
+    "url": "/react-cart/static/js/runtime-main.8c787756.js"
+  }
+]);
